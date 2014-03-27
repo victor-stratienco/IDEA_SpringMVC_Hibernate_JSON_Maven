6591c6cdfddccba3e4bad245afdd2e1c9000df0d
Victor Stratienco <victor.stratienco@phenogenomics.ca>
2014-03-27 11:04:34 -0400

    - CRUD + JSON

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -5,12 +5,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-@RequestMapping("/")
+//@Controller
+//@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello World!");
-		return "hello";
-	}
+//	@RequestMapping(method = RequestMethod.GET)
+//	public String printWelcome(ModelMap model) {
+//		model.addAttribute("message", "Hello World!");
+//		return "hello";
+//	}
 }
